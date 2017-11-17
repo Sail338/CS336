2172a25d49d8ad0463902d4b78a2434e3df21e07
Sam Azouzi <sazouzi21@gmail.com>
2017-11-16 19:11:00 -0500

    routes added

--- hotel/view.py
+++ hotel/view.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import render_template,request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('index.html')
+
+
+@app.route('/registration')
+
+@app.route('/register')
+
+@app.route('/profile')
+
+@app.route('/profile-edit')
+
+@app.route('/browse')
+
+@app.route('/reserve')
+
+@app.route('/registerHotel')
