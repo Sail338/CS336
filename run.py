124306c3f267b6789878345473ee7e8e3ef8e0d5
Rui-Zhang1997 <r.zhang194@gmail.com>
2017-11-20 02:13:59 -0500

    Add requirements.txt and .gitignore. Modifies run.py to run.

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-from santa import app
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/ping')
+def index():
+    return 'pong'
+
 if __name__ == "__main__":
       app.run(port=5000, debug=True)
