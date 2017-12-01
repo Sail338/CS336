9bb47e2cbec75c63db033ad32578be902adb46c3
Yash Sanghavi <ysanghavi1@gmail.com>
2017-12-01 16:51:06 -0500

    login page styled

--- hotel/scripts/login.js
+++ hotel/scripts/login.js
@@ -1,26 +1,7 @@
-function deselect(e) {
-  $('.pop').slideFadeToggle(function() {
-    e.removeClass('selected');
-  });    
-}
-
-$(function() {
-  $('#user').on('click', function() {
-    if($(this).hasClass('selected')) {
-      deselect($(this));               
-    } else {
-      $(this).addClass('selected');
-      $('.pop').slideFadeToggle();
-    }
-    return false;
-  });
-
-  $('.close').on('click', function() {
-    deselect($('#user'));
-    return false;
-  });
+$("#user").click(function() {
+  $(".modal").addClass("is-active");  
 });
 
-$.fn.slideFadeToggle = function(easing, callback) {
-  return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
-};
+$(".modal-close").click(function() {
+   $(".modal").removeClass("is-active");
+});
