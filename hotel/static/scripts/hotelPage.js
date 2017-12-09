354fb075790b5d0fec901e3d6bb58e4c7970dc9c
Yash Sanghavi <ysanghavi1@gmail.com>
2017-12-09 00:48:32 -0500

    displaying reviews properly

--- hotel/static/scripts/hotelPage.js
+++ hotel/static/scripts/hotelPage.js
@@ -1,8 +1,18 @@
 $(".tag").click(function() {
-	console.log($($(this).next()).find("div"));
+	$($(this).next()).css("display", "inherit")
   $($(this).next()).find("div").addClass("is-active");  
 });
 
 $(".modal-close").click(function() {
+	$($(this).parent()).parent().css("display", "none");
    $(".modal").removeClass("is-active");
+});
+
+$(".reviewButton").click(function(){
+	if($($($($($(this).parent()).parent()).parent()).next()).css("display")==="none"){
+		$($($($($(this).parent()).parent()).parent()).next()).css("display", "inherit");
+	}else{
+		$($($($($(this).parent()).parent()).parent()).next()).css("display", "none");
+	}
+	
 });
